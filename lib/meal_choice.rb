ee957677b00b517409bf392b3e1c24d4ad89ace4
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-24 19:31:10 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,8 @@
 
 def meal_choice (veg1, veg2, protien="meat")
   puts "What a nutritious meal!"
-  meal = A plate of #{protien} with #{veg1} and #{veg2}."
+  meal = "A plate of #{protien} with #{veg1} and #{veg2}."
+  puts meal
 end 
 
 
